0bcbf34740a82d8adbe48f735f2089e047c5fb72
Andres-Andretti <acorredor407@gmail.com>
2021-05-12 16:38:25 -0400

    fixed mongo

--- server/routes/route.posts.js
+++ server/routes/route.posts.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const mongoose = require('mongoose')
 
-const { getPosts } = require('../controllers/controller.posts.js')
+const { getPosts, createPost } = require('../controllers/controller.posts.js')
 
 const router = express.Router();
 
 router.get('/', getPosts);
+router.post('/', createPost)
 
 
 module.exports = router; 
