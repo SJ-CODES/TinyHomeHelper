c34eb09545514b953a14416740e175ec170f052b
mmason27 <mmmason27@gmail.com>
2021-05-12 14:54:38 -0400

    fixed routing bug

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
-import React,{Component} from 'react'
-import TinyHomeChecklist from './components/TinyHomeChecklist'
-import UploadForm from './components/UploadForm';
+import './Components/CSS/App.css';
+import React, {Component} from 'react'
+import TinyHomeChecklist from './Components/TinyHomeChecklist'
+import UploadForm from './Components/UploadForm';
+import "bootswatch/dist/minty/bootstrap.min.css";
+import './Components/CSS/Index.css';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from './Components/Home';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import AboutUs from "./Components/AboutUs";
+// import TinyHomeChecklist from "./Components/TinyHomeChecklist"
+import PhotoGallery from "./Components/PhotoGallery"
+// import UploadForm from './Components/UploadForm';
+import CommunityForum from './Components/CommunityForum';
+
 function App() {
   return (
     
     <div className="App">
-      <h1>Capstone Project</h1>
-      <TinyHomeChecklist />
+      
+      <BrowserRouter> 
+          <Switch>
+              <Route exact path="/" component={App}>
+                  <Header/>
+                  <Home/>
+                  <Footer/>
+                </Route>
+                <Route exact path="/about-us" component={AboutUs}>
+                  <Header/>
+                  <AboutUs/>
+                  <Footer/>
+                </Route>
+                <Route exact path="/TinyHomeChecklist" component= {TinyHomeChecklist}>
+                  <Header/>
+                </Route>
+                <Route exact path ="/PhotoGallery" component= {PhotoGallery}>
+                  <Header/>
+                  <UploadForm />
+                </Route>
+                  <Route exact path="/community-forum">
+                  <CommunityForum/>
+                  <Footer/>
+                </Route>
+          </Switch>
+      </BrowserRouter>
+
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,48 +1,14 @@
 import React from 'react';
 import "bootswatch/dist/minty/bootstrap.min.css";
 import ReactDOM from 'react-dom';
-
-import './index.css';
+import './Components/CSS/Index.css';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import Home from './components/Home';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import AboutUs from "./components/AboutUs";
-import TinyHomeChecklist from "./components/TinyHomeChecklist"
-import PhotoGallery from "./components/PhotoGallery"
 import App from './App'
-import UploadForm from './components/UploadForm';
-import CommunityForum from './Components/CommunityForum';
 
 ReactDOM.render(
   <React.StrictMode>
-      <BrowserRouter>
-            
-          <Switch>
-              <Route exact path="/" component={App}>
-                  <Header/>
-                  <Home/>
-                  <Footer/>
-                </Route>
-                <Route exact path="/about-us"component={AboutUs} >
-                  <Header/>
-                  <AboutUs/>
-                  <Footer/>
-                </Route>
-                <Route exact path="/TinyHomeChecklist" component= {TinyHomeChecklist}>
-                  <Header/>
-                </Route>
-                <Route exact path ="/PhotoGallery" component= {PhotoGallery}>
-                  <Header/>
-                  <UploadForm />
-                </Route>
-                  <Route exact path="/community-forum">
-                  <CommunityForum/>
-                  <Footer/>
-                </Route>
-          </Switch>
-      </BrowserRouter>
+
+    <App/>
 
   </React.StrictMode>,
   document.getElementById('root')
