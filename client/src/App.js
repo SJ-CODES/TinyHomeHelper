544ef8e8d3b62e9fef6579d403320e0cbd4cdc9a
LaytonAvery <laytonavery5@gmail.com>
2021-05-12 17:08:21 -0400

    from Index to index

--- client/src/App.js
+++ client/src/App.js
@@ -1,52 +1,49 @@
-import './Components/CSS/App.css';
-import React, {Component} from 'react'
-import TinyHomeChecklist from './Components/TinyHomeChecklist'
-import UploadForm from './Components/UploadForm';
+import "./Components/CSS/App.css";
+import React, { Component } from "react";
+import TinyHomeChecklist from "./Components/TinyHomeChecklist";
+import UploadForm from "./Components/UploadForm";
 import "bootswatch/dist/minty/bootstrap.min.css";
-import './Components/CSS/Index.css';
+import "./Components/CSS/index.css";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import Home from './Components/Home';
-import Header from './Components/Header';
-import Footer from './Components/Footer';
+import Home from "./Components/Home";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
 import AboutUs from "./Components/AboutUs";
 // import TinyHomeChecklist from "./Components/TinyHomeChecklist"
-import PhotoGallery from "./Components/PhotoGallery"
+import PhotoGallery from "./Components/PhotoGallery";
 // import UploadForm from './Components/UploadForm';
-import CommunityForum from './Components/CommunityForum';
+import CommunityForum from "./Components/CommunityForum";
 
 function App() {
-  return (
-    
-    <div className="App">
-      
-      <BrowserRouter> 
-          <Switch>
-              <Route exact path="/" component={App}>
-                  <Header/>
-                  <Home/>
-                  <Footer/>
-                </Route>
-                <Route exact path="/about-us" component={AboutUs}>
-                  <Header/>
-                  <AboutUs/>
-                  <Footer/>
-                </Route>
-                <Route exact path="/TinyHomeChecklist" component= {TinyHomeChecklist}>
-                  <Header/>
-                </Route>
-                <Route exact path ="/PhotoGallery" component= {PhotoGallery}>
-                  <Header/>
-                  <UploadForm />
-                </Route>
-                  <Route exact path="/community-forum">
-                  <CommunityForum/>
-                  <Footer/>
-                </Route>
-          </Switch>
-      </BrowserRouter>
-
-    </div>
-  );
+	return (
+		<div className='App'>
+			<BrowserRouter>
+				<Switch>
+					<Route exact path='/' component={App}>
+						<Header />
+						<Home />
+						<Footer />
+					</Route>
+					<Route exact path='/about-us' component={AboutUs}>
+						<Header />
+						<AboutUs />
+						<Footer />
+					</Route>
+					<Route exact path='/TinyHomeChecklist' component={TinyHomeChecklist}>
+						<Header />
+					</Route>
+					<Route exact path='/PhotoGallery' component={PhotoGallery}>
+						<Header />
+						<UploadForm />
+					</Route>
+					<Route exact path='/community-forum'>
+						<CommunityForum />
+						<Footer />
+					</Route>
+				</Switch>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
+import React from "react";
 import "bootswatch/dist/minty/bootstrap.min.css";
-import ReactDOM from 'react-dom';
-import './Components/CSS/Index.css';
-import reportWebVitals from './reportWebVitals';
-import App from './App'
+import ReactDOM from "react-dom";
+import "./Components/CSS/index.css";
+import reportWebVitals from "./reportWebVitals";
+import App from "./App";
 
 ReactDOM.render(
-  <React.StrictMode>
-
-    <App/>
-
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
