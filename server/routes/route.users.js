2f9da03813141795562da2b309b4eb6b82cb3530
Andres-Andretti <acorredor407@gmail.com>
2021-05-12 12:22:51 -0400

    fixed

--- server/routes/route.users.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const { signin } = require("../controllers/controller.user");
-
-const router = express.Router();
-
-router.post("/signin", signin);
-
-module.exports = router;
