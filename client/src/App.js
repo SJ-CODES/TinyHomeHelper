c7f612c38a8fa4bf6a161d551c1bd15107b939b3
mmason27 <mmmason27@gmail.com>
2021-05-17 11:52:52 -0400

    fixed bugs

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 
 import PhotoGalleryDisplay from "./Components/PhotoGalleryDisplay"
-import CommunityForum from './Components/CommunityForum';
 import ForumHeader from './Components/ForumHeader';
 import './Components/CSS/PhotoGallery.css'
 import LoginScreen from "./Components/LoginScreen";
@@ -8,7 +7,7 @@ import RegisterScreen from "./Components/RegisterScreen";
 import CommunityForum from "./Components/CommunityForum";
 import EducateHomepage from "./Components/EducateHomepage";
 import "bootswatch/dist/minty/bootstrap.min.css";
-import "./Components/CSS/index.css";
+import "./Components/CSS/Index.css";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./Components/Home";
 import Header from "./Components/Header";
--- client/src/reducers/index.js
+++
@@ -1,5 +0,0 @@
-import { combineReducers } from 'redux'
-
-import posts from '../store/postsReducer'
-
-export const reducers = combineReducers({ posts })
